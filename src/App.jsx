efcdbdf07c2e8b1262f4d7953e5c57f7089650ab
ikxneng <97749494+ikxneng@users.noreply.github.com>
2024-10-12 21:48:10 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavBar from './components/NavBar'
 import Home from './components/Home';
+import Footer from './components/Footer'
 import MoviesPage from './components/MoviesPage'
 import PopularPage from './components/PopularPage'
 import TrailersPage from './components/TrailersPage'
+import MovieDetails from './components/MovieDetails';
 
 function App() {
   return(
@@ -13,13 +15,14 @@ function App() {
     <Router>
             <div className="App">
               <NavBar />
-              
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/movies" element={<MoviesPage />} />
                     <Route path="/popular" element={<PopularPage />} />
                     <Route path="/trailers" element={<TrailersPage />} />
+                    <Route path="/details/:title" element={<MovieDetails />} />
                 </Routes>
+                <Footer />
             </div>
         </Router>
   )
