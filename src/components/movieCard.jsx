ab6abe6d0e2b72cf749f3a3022ff12d07d15b7d8
ikxneng <97749494+ikxneng@users.noreply.github.com>
2024-10-17 14:38:38 +0200

    add movieCard

---
+++ src/components/movieCard.jsx
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const MovieCard = ({ title, image, year, description, cast, genre, rating, link }) => {
+  return (
+    <div className="space-x-16">
+      <Link to={`/details/${title}`}>
+        <div className="w-[375px] h-[195px] overflow-hidden rounded-md">
+          <img src={image} alt={title} className="w-full h-full object-cover rounded-md" />
+        </div>
+        <p className="text-black mt-2 font-semibold text-left">{title}</p>
+        {year && <p className="text-black font-light text-left">{year}</p>}
+      </Link>
+    </div>
+  );
+};
+
+export default MovieCard;
