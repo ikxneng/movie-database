11803e5439e9ff41dd82bbc93648891d3ab1c41b
ikxneng <97749494+ikxneng@users.noreply.github.com>
2024-10-17 15:38:07 +0200

    Update MovieDetails.jsx

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -9,275 +9,146 @@ function MovieDetails() {
 
     // Movie data array with images and titles
     const movieData = [ 
-        {
-        "image": "/src/assets/thegodfather.jpg",
+      {
+        "image": "assets/thegodfather.jpg",
         "title": "The Godfather",
         "rating": "4.7",
         "link": "https://youtu.be/w3Wo6QiD3eU?si=dH71_QbmlnszYROL",
         "description": "The patriarch of an organized crime dynasty transfers control of his empire to his reluctant son.",
-        "cast": ["Marlon Brando, ", "Al Pacino, ", "James Caan"],
+        "cast": ["Marlon Brando", "Al Pacino", "James Caan"],
         "genre": "Crime Drama"
       },
       {
-        "image": "/src/assets/forrestgump.jpg",
+        "image": "assets/forrestgump.jpg",
         "title": "Forrest Gump",
         "rating": "4.0",
         "link": "https://youtu.be/bLvqoHBptjg?si=ihHqUDx1HOz75bWk",
         "description": "The story of a man with a low IQ who achieves extraordinary things in his life.",
-        "cast": ["Tom Hanks, ", "Robin Wright, ", "Gary Sinise"],
+        "cast": ["Tom Hanks", "Robin Wright", "Gary Sinise"],
         "genre": "Drama Romance"
       },
       {
-        "image": "/src/assets/thelionking.jpeg",
+        "image": "assets/thelionking.jpeg",
         "title": "The Lion King",
         "rating": "4.7",
         "link": "https://youtu.be/lFzVJEksoDY?si=IhIK5SGFYVfkrbxn",
         "description": "A young lion prince flees his kingdom only to learn the true meaning of responsibility and bravery.",
-        "cast": ["Matthew Broderick, ", "Jeremy Irons, ", "James Earl Jones"],
+        "cast": ["Matthew Broderick", "Jeremy Irons", "James Earl Jones"],
         "genre": "Animation Adventure"
       },
       {
-        "image": "/src/assets/werethemillers.jpg",
+        "image": "assets/werethemillers.jpg",
         "title": "We're the Millers",
-        "year": "2013",
         "description": "A small-time pot dealer creates a fake family as part of his plan to smuggle drugs into the U.S.",
-        "cast": ["Jason Sudeikis", "Jennifer Aniston", "Emma Roberts"].join(', '),
+        "cast": ["Jason Sudeikis", "Jennifer Aniston", "Emma Roberts"],
         "genre": "Comedy",
         "rating": "3.5"
       },
       {
-        "image": "/src/assets/metime.jpg",
+        "image": "assets/metime.jpg",
         "title": "Me Time",
-        "year": "2023",
         "description": "A stay-at-home dad's wild weekend with an old friend spirals out of control.",
-        "cast": ["Kevin Hart", "Mark Wahlberg", "Regina Hall"].join(', '),
+        "cast": ["Kevin Hart", "Mark Wahlberg", "Regina Hall"],
         "genre": "Comedy",
         "rating": "2.5"
       },
-  
       {
-        "image": "/src/assets/Green-Book.jpg",
+        "image": "assets/Green-Book.jpg",
         "title": "Green Book",
-        "year": "2018",
         "description": "A tour through the Deep South with an African American classical pianist and his driver.",
-        "cast": ["Viggo Mortensen", "Mahershala Ali", "Linda Cardellini"].join(', '),
+        "cast": ["Viggo Mortensen", "Mahershala Ali", "Linda Cardellini"],
         "genre": "Drama",
         "rating": "4.1"
       },
       {
-        "image": "/src/assets/Rush Hour.jpg",
+        "image": "assets/Rush Hour.jpg",
         "title": "Rush Hour",
-        "year": "1998",
         "description": "A Hong Kong detective teams up with an LAPD officer to rescue the Chinese Consul's kidnapped daughter.",
-        "cast": ["Jackie Chan", "Chris Tucker", "Tom Wilkinson"].join(', '),
+        "cast": ["Jackie Chan", "Chris Tucker", "Tom Wilkinson"],
         "genre": "Action, Comedy",
         "rating": "3.5"
       },
       {
-        "image": "/src/assets/Union.jpg",
+        "image": "assets/Union.jpg",
         "title": "Union",
-        "year": "2024",
-        "description": "Construction worker Mike is thrust into the world of espionage when his high school sweetheart, Roxanne, recuits him for a high-stake intelligence mission",
-        "cast": ["Halle Berry", "Mark Wahlberg"].join(', '),
+        "description": "Construction worker Mike is thrust into the world of espionage when his high school sweetheart, Roxanne, recruits him for a high-stakes intelligence mission.",
+        "cast": ["Halle Berry", "Mark Wahlberg"],
         "genre": "Sci-Fi, Thriller",
         "rating": "2.4"
       },
       {
-        "image": "/src/assets/bladerunner.jpg",
+        "image": "assets/bladerunner.jpg",
         "title": "Blade Runner 2049",
-        "year": "2017",
         "description": "A young blade runner discovers a long-buried secret that leads him to track down former blade runner Rick Deckard.",
-        "cast": ["Ryan Gosling", "Harrison Ford", "Ana de Armas"].join(', '),
+        "cast": ["Ryan Gosling", "Harrison Ford", "Ana de Armas"],
         "genre": "Sci-Fi, Action",
         "rating": "4"
       },
       {
-        "image": "/src/assets/Black-Adam.jpg",
+        "image": "assets/Black-Adam.jpg",
         "title": "Black Adam",
-        "year": "2022",
         "description": "A former stuntman returns to Hollywood for a comeback, facing old foes and new dangers.",
-        "cast": ["Dwyane 'The Rock' Johnson", "Aldis Hodge", "Pierce Brosnan"].join(', '),
+        "cast": ["Dwyane 'The Rock' Johnson", "Aldis Hodge", "Pierce Brosnan"],
         "genre": "Action, Comedy",
         "rating": "3"
       },
       {
-        "image": "/src/assets/werethemillers.jpg",
-        "title": "We're the Millers",
-        "year": "2013",
-        "description": "A small-time pot dealer creates a fake family as part of his plan to smuggle drugs into the U.S.",
-        "cast": ["Jason Sudeikis", "Jennifer Aniston", "Emma Roberts"].join(', '),
-        "genre": "Comedy",
-        "rating": "3.5"
-      },
-      {
-        "image": "/src/assets/metime.jpg",
-        "title": "Me Time",
-        "year": "2023",
-        "description": "A stay-at-home dad's wild weekend with an old friend spirals out of control.",
-        "cast": ["Kevin Hart", "Mark Wahlberg", "Regina Hall"].join(', '),
-        "genre": "Comedy",
-        "rating": "5.0"
-      },
-      {
-        "image": "/src/assets/pinkpanther2.jpg",
+        "image": "assets/pinkpanther2.jpg",
         "title": "Pink Panther 2",
-        "year": "2009",
         "description": "Inspector Clouseau and his bumbling team of detectives chase down a master thief.",
-        "cast": ["Steve Martin", "Jean Reno", "Emily Mortimer"].join(', '),
+        "cast": ["Steve Martin", "Jean Reno", "Emily Mortimer"],
         "genre": "Comedy, Crime",
         "rating": "2.8"
       },
       {
-        "image": "/src/assets/crazystupidlove.jpg",
+        "image": "assets/crazystupidlove.jpg",
         "title": "Crazy, Stupid, Love",
-        "year": "2011",
         "description": "A middle-aged man learns how to date again after his marriage falls apart.",
-        "cast": ["Steve Carell", "Ryan Gosling", "Emma Stone"].join(', '),
+        "cast": ["Steve Carell", "Ryan Gosling", "Emma Stone"],
         "genre": "Comedy, Romance",
         "rating": "3.7"
       },
       {
-        "image": "/src/assets/fallguymovie-poster.jpg",
+        "image": "assets/fallguymovie-poster.jpg",
         "title": "Fall Guy",
-        "year": "2023",
         "description": "A former stuntman returns to Hollywood for a comeback, facing old foes and new dangers.",
-        "cast": ["Ryan Gosling", "Emma Blunt"].join(', '),
+        "cast": ["Ryan Gosling", "Emma Blunt"],
         "genre": "Action, Comedy",
         "rating": "3"
       },
       {
-        "image": "/src/assets/dunept2.jpg",
+        "image": "assets/dunept2.jpg",
         "title": "Dune: Part Two",
-        "year": "2024",
         "description": "Paul Atreides unites with the Fremen to take vengeance against the conspirators who destroyed his family.",
-        "cast": ["Timothée Chalamet", "Zendaya", "Rebecca Ferguson"].join(', '),
+        "cast": ["Timothée Chalamet", "Zendaya", "Rebecca Ferguson"],
         "genre": "Sci-Fi, Adventure, Drama",
         "rating": "3"
       },
       {
-        "image": "/src/assets/Green-Book.jpg",
-        "title": "Green Book",
-        "year": "2018",
-        "description": "A tour through the Deep South with an African American classical pianist and his driver.",
-        "cast": ["Viggo Mortensen", "Mahershala Ali", "Linda Cardellini"].join(', '),
-        "genre": "Drama",
-        "rating": "4.1"
-      },
-      {
-        "image": "/src/assets/shesaid.jpg",
+        "image": "assets/shesaid.jpg",
         "title": "She Said",
-        "year": "2022",
         "description": "Two New York Times reporters break one of the most important stories in a generation — a story that helped ignite a movement.",
-        "cast": ["Carey Mulligan", "Zoe Kazan", "Patricia Clarkson"].join(', '),
+        "cast": ["Carey Mulligan", "Zoe Kazan", "Patricia Clarkson"],
         "genre": "Drama, History",
         "rating": "3.6"
       },
       {
-        "image": "/src/assets/the-unforgivable.jpeg",
+        "image": "assets/the-unforgivable.jpeg",
         "title": "The Unforgivable",
-        "year": "2021",
         "description": "A woman attempts to rebuild her life after serving a prison sentence for a violent crime.",
-        "cast": ["Sandra Bullock", "Viola Davis", "Rob Morgan"].join(', '),
+        "cast": ["Sandra Bullock", "Viola Davis", "Rob Morgan"],
         "genre": "Drama, Crime",
         "rating": "3.5"
       },
       {
-        "image": "/src/assets/frey-allen-planet-of-the-apes-banner-1.jpg",
+        "image": "assets/frey-allen-planet-of-the-apes-banner-1.jpg",
         "title": "Kingdom of the Planet of the Apes",
-        "year": "2024",
         "description": "In a world ruled by apes, humans struggle for survival as new alliances and enemies emerge.",
-        "cast": ["Freya Allan", "Kevin Durand", "Peter Macon"].join(', '),
-        "genre": "Sci-Fi, Action, Drama",
-        "rating": "4"
-      },
-      {
-        "image": "/src/assets/thegodfather.jpg",
-        "title": "The Godfather",
-        "rating": "4.7",
-        "link": "https://youtu.be/w3Wo6QiD3eU?si=dH71_QbmlnszYROL",
-        "description": "The patriarch of an organized crime dynasty transfers control of his empire to his reluctant son.",
-        "cast": ["Marlon Brando", "Al Pacino", "James Caan"].join(', '),
-        "genre": "Crime, Drama"
-      },
-      {
-        "image": "/src/assets/forrestgump.jpg",
-        "title": "Forrest Gump",
-        "rating": "4.0",
-        "link": "https://youtu.be/bLvqoHBptjg?si=ihHqUDx1HOz75bWk",
-        "description": "The story of a man with a low IQ who achieves extraordinary things in his life.",
-        "cast": ["Tom Hanks", "Robin Wright", "Gary Sinise"].join(', '),
-        "genre": "Drama, Romance"
-      },
-      {
-        "image": "/src/assets/thelionking.jpeg",
-        "title": "The Lion King",
-        "rating": "4.7",
-        "link": "https://youtu.be/lFzVJEksoDY?si=IhIK5SGFYVfkrbxn",
-        "description": "A young lion prince flees his kingdom only to learn the true meaning of responsibility and bravery.",
-        "cast": ["Matthew Broderick", "Jeremy Irons", "James Earl Jones"].join(', '),
-        "genre": "Animation, Adventure"
-      },
-      {
-        "id": 1,
-        "title": "Challengers",
-        "year": 2024,
-        "image": "/src/assets/Popular/challengers.jpg",
-        "genre": "Sports Drama, Romance",
-        "rating": "Not yet rated",
-        "description": "The movie follows the rise and fall of three tennis players competing for both personal and professional wins in a world where love and sports collide.",
-        "cast": ["Zendaya", "Mike Faist", "Josh O'Connor"].join(', ')
-    },
-    {
-        "id": 2,
-        "title": "Bad Boys 4",
-        "year": 2024,
-        "image": "/src/assets/Popular/bad-boys-4 1.png",
-        "genre": "Action, Comedy",
-        "rating": "Not yet rated",
-        "description": "Detectives Mike Lowrey and Marcus Burnett return, now fugitives, facing conspiracies and threats from a new villain. They team up with AMMO to clear their names and bring justice.",
-        "cast": ["Will Smith", "Martin Lawrence", "Vanessa Hudgens", "Eric Dane", "Paola Núñez"].join(', ')
-    },
-    {
-        "id": 3,
-        "title": "Deadpool and Wolverine",
-        "year": 2024,
-        "image": "/src/assets/Popular/DeadpoolVsWolverine.jpg",
-        "genre": "Action, Comedy, Superhero",
-        "rating": "Not yet rated",
-        "description": "This crossover pits Deadpool and Wolverine against formidable foes as they navigate the chaotic dynamics of their unlikely partnership.",
-        "cast": ["Ryan Reynolds", "Hugh Jackman"].join(', ')
-    },
-    {
-        "id": 4,
-        "title": "Blink Twice",
-        "year": 2024,
-        "image": "/src/assets/Popular/BlinkTwice.jpg",
-        "genre": "Thriller",
-        "rating": "Not yet rated",
-        "description": "The plot details are currently under wraps, but the film is anticipated to be a suspenseful thriller revolving around unexpected twists and intense character drama.",
-        "cast": ["Channing Tatum", "Adria Arjona", "Naomi Ackie"].join(', ')
-    },
-    {
-        "id": 5,
-        "title": "Uglies",
-        "year": 2024,
-        "image": "/src/assets/Popular/Uglies.jpg",
-        "genre": "Sci-Fi, Young Adult",
-        "rating": "Not yet rated",
-        "description": "Based on the novel, the film explores a dystopian world where everyone undergoes cosmetic surgery at 16 to become 'pretty,' delving into societal expectations and the cost of conformity.",
-        "cast": ["Joey King", "Chase Stokes"].join(', ')
-    },
-    {
-        "id": 6,
-        "title": "Transformers One",
-        "year": 2024,
-        "image": "/src/assets/Popular/Transformers.png",
-        "genre": "Animation, Action, Sci-Fi",
-        "rating": "Not yet rated",
-        "description": "An origin story for the Transformers, this animated feature dives into the history of Optimus Prime and Megatron before they became bitter enemies.",
-        "cast": ["Chris Hemsworth", "Scarlett Johansson"].join(', ')
-    }
-    ]
+        "cast": ["Freya Allan", "Kevin Durand", "Peter Macon"],
+        "genre": "Sci-Fi, Action",
+        "rating": "2.9"
+      }
+    ];
     
     useEffect(() => {
         setLoading(true);
