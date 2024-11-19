252461974143ba0a966201611c7549a78d9fe423
ikxneng <97749494+ikxneng@users.noreply.github.com>
2024-11-19 21:04:00 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-const plugin = require('tailwind-scrollbar')
+
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [
-    require ('tailwind-scrollbar'),
-  ],
-  variants: {
-    scrollbar: ['rounded'], // Enable scrollbar variants (e.g., rounded corners)
-  },
+  plugins: [],
 };
