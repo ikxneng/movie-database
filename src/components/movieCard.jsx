d37d5ddc3f40438447439fc3d6eb36324ec183cb
ikxneng <97749494+ikxneng@users.noreply.github.com>
2024-11-21 11:45:53 +0200

    Update movieCard.jsx

--- src/components/movieCard.jsx
+++ src/components/movieCard.jsx
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom';
 
 const MovieCard = ({ title, image, year, description, cast, genre, rating, link }) => {
   return (
-    <div className="space-x-16">
+    <div 
+    className="space-x-16 transform transition-all duration-300 hover:scale-105 hover:-translate-y-2"
+    >
       <Link to={`/details/${title}`}>
         <div className="w-[375px] h-[195px] overflow-hidden rounded-md">
           <img src={image} alt={title} className="w-full h-full object-cover rounded-md" />
