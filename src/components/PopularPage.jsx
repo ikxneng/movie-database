1f68bdc8d3c7ebe8e576c9914af452e1d5ba4f23
ikxneng <97749494+ikxneng@users.noreply.github.com>
2024-11-21 11:58:55 +0200

    Update PopularPage.jsx

--- src/components/PopularPage.jsx
+++ src/components/PopularPage.jsx
@@ -48,7 +48,9 @@ function PopularPage() {
             {/* Movies Grid */}
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
                 {movies.map((movie) => (
-                    <div key={movie.id} className="bg-white overflow-hidden">
+                    <div key={movie.id} 
+                    className="bg-white overflow-hidden transform transition-all duration-300 hover:scale-105 hover:-translate-y-2"
+                    >
                         <Link to={`/details/${movie.title}`}>
                         <div className="w-full overflow-hidden">
                             <img
